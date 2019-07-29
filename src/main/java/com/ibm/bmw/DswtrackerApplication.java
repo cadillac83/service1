d3a0ad67e8af1a8c1fb87429bc9a4b4ca9662756
liuqdl <liuqdl@cn.ibm.com>
2019-07-29 21:35:05 +0800

    init pro

---
+++ src/main/java/com/ibm/bmw/DswtrackerApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.bmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswtrackerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ibm/bmw/MainConfig.java
@@ -0,0 +1,44 @@
+package com.ibm.bmw;
+
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/******************************************************************
+ *
+ *    Java Lib, Powered By Dalian IBM.
+ *
+ *    Copyright (c) 2001-2018 Dalian IBM Co.,Ltd
+ *    http://www.ibm.com/
+ *
+ *    Package:     com.ibm.mail
+ *
+ *    Filename:    ${file_name}
+ *
+ *    Description: Spring Boot的主配置，这个配置是基本所有都要用的配置
+ *
+ *    Copyright:   Copyright (c) 2001-2018
+ *
+ *    Company:     Dalian IBM Co.,Ltd
+ *
+ *    @author: LiuQiang
+ *
+ *    @version: 1.0.0
+ *
+ *    Create at:   5/21/2017 1:29 PM
+ *
+ *    Revision:
+ *
+ *    5/21/2017 1:29 PM
+ *        - first revision
+ *
+ *****************************************************************/
+
+@Configuration   //@Configuration注解的作用是表示这个类是一个配置类
+@ComponentScan({"com.ibm.bmw.*"})   //包扫描的位置
+public class MainConfig {
+}
